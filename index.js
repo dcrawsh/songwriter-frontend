a1ba02e050c3de1fdbaf0df2c8939254e751dca9
dcrawsh <dcrawsh@gmail.com>
2020-06-07 19:48:47 -0700

    implemented get fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello')
+const endPoint = "http://localhost:3000/api/v1/songs"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getSongs();
+})
+
+function getSongs() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(songs => {
+        songs.data.forEach(song => {
+            
+           const songMarkup = `<h3> ${song.attributes.name} </h3
+           <h3> ${song.attributes.lyrics} </h3>
+           <h3> ${song.attributes.chords} </h3`
+
+
+
+           document.querySelector("#song-container").innerHTML += songMarkup
+            
+            
+            
+            
+            
+
+           
+        })
+    })
+}
